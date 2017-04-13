c4da5bc73b8e22550352d0ac4469daace93aa8bf
Mike Anthony <mikeanthony1991@gmail.com>
2017-04-14 00:11:24 +0800

    added bargraph

--- index.php
+++ index.php
@@ -29,6 +29,13 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+	
+	<!--Declare our PHP Database Connection-->
+	<?php
+	$conn_string = "host=localhost port=5432 dbname=gnucash user=gnucash password=postPASS101";
+	$dbconn = pg_connect($conn_string);
+	?>
+	
 
 </head>
 
@@ -46,7 +53,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">SB Admin</a>
+                <a class="navbar-brand" href="index.html">GnuCash Accounting</a>
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
@@ -131,7 +138,7 @@
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Mike Anthony <b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li>
                             <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
@@ -200,7 +207,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Dashboard <small>Statistics Overview</small>
+                            Dashboard <small>Finance Overview</small>
                         </h1>
                         <ol class="breadcrumb">
                             <li class="active">
@@ -211,61 +218,15 @@
                 </div>
                 <!-- /.row -->
                 <div class="row">
-                    <div class="col-lg-4">
+                    <div class="col-lg-8">
                         <div class="panel panel-default">
                             <div class="panel-heading">
                                 <h3 class="panel-title"><i class="fa fa-long-arrow-right fa-fw"></i>Expenses</h3>
                             </div>
                             <div class="panel-body">
-                                <div id="donut-example"></div>
+                                <div id="expense-bar"></div>
                                 <div class="text-right">
-                                    <a href="#">View Details <i class="fa fa-arrow-circle-right"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h3 class="panel-title"><i class="fa fa-clock-o fa-fw"></i> Tasks Panel</h3>
-                            </div>
-                            <div class="panel-body">
-                                <div class="list-group">
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">just now</span>
-                                        <i class="fa fa-fw fa-calendar"></i> Calendar updated
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">4 minutes ago</span>
-                                        <i class="fa fa-fw fa-comment"></i> Commented on a post
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">23 minutes ago</span>
-                                        <i class="fa fa-fw fa-truck"></i> Order 392 shipped
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">46 minutes ago</span>
-                                        <i class="fa fa-fw fa-money"></i> Invoice 653 has been paid
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">1 hour ago</span>
-                                        <i class="fa fa-fw fa-user"></i> A new user has been added
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">2 hours ago</span>
-                                        <i class="fa fa-fw fa-check"></i> Completed task: "pick up dry cleaning"
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">yesterday</span>
-                                        <i class="fa fa-fw fa-globe"></i> Saved the world
-                                    </a>
-                                    <a href="#" class="list-group-item">
-                                        <span class="badge">two days ago</span>
-                                        <i class="fa fa-fw fa-check"></i> Completed task: "fix error on sales page"
-                                    </a>
-                                </div>
-                                <div class="text-right">
-                                    <a href="#">View All Activity <i class="fa fa-arrow-circle-right"></i></a>
+                                    <a href="#">View Details<i class="fa fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -279,9 +240,6 @@
                                 <div class="table-responsive">
                                     <table class="table table-bordered table-hover table-striped">
                                         <?php
-											$conn_string = "host=localhost port=5432 dbname=gnucash user=gnucash password=postPASS101";
-											$dbconn = pg_connect($conn_string);
-											
 											echo("<table class='table table-striped table-hover table-responsive' border=2>\n");
 											echo("<thead class='thead-default'>");
 											echo("<tr><th>Date</th><th>Description</th><th>Amount</th></tr>");
@@ -289,8 +247,7 @@
 											$query = "select guid,post_date,description from public.transactions ORDER BY post_date DESC";
 											$res_trans = pg_query($dbconn, $query);
 											echo("<tbody>");
-											$gnuChartArray = array();
-											$tempArray = array();
+											
 											#Grab an array of rows of transactions, then create a table entry for each row
 											while ($row = pg_fetch_row($res_trans)) {
 												
@@ -302,41 +259,15 @@
 												echo("<td>$row[2]</td>");
 												
 												#Get the value for the transation
-												$query = "select value_num,value_denom,account_guid from public.splits WHERE tx_guid = ('$row[0]')";
+												$query = "select value_num,value_denom from public.splits WHERE tx_guid = ('$row[0]')";
 												
-												#res_splits now contains a list of values and account guids for each transaction
+												#res_splits now contains a list of values for each transaction
 												$res_splits = pg_query($dbconn,$query);
 												
-												#Grab the value and account name, add it to the table
+												#Grab the value and add it to the current table row
 												while ($sec_row = pg_fetch_row($res_splits)) {
-													$query = "select name from public.accounts WHERE guid = ('$sec_row[2]') AND account_type = ('EXPENSE')";
-
-													#res_acct now contains a list of account names
-													$res_acct = pg_query($dbconn,$query);
-													#Iterate through list of account names
-													while ($third_row = pg_fetch_row($res_acct)) {
-														
-														#Set our label for our table data from account name
-														$tempArray['label']= "$third_row[0]";
-														
-														#Grab the total value for each account for donut graph
-														$query = "select value_num from public.splits WHERE account_guid = ('$sec_row[2]')";
-														
-														#res_acct_vals now contains a single value for the current account selected.
-														$res_acct_vals = pg_query($dbconn,$query);
-														$acc_vals = 0;
-														while ($fourth_row = pg_fetch_row($res_acct_vals)) {
-															if ((int)$fourth_row[0] > 0){
-																$acc_vals += $fourth_row[0];
-															}
-														}
-														$acc_vals = $acc_vals/100;
-														
-														#Now we set the value key of tempArray to the total value of the account.
-														$tempArray['value']= "$acc_vals";
-														$gnuChartArray[] = $tempArray;
-													}
-													#Show the non-summed value for each account
+													
+													#Only show the positive values for each account (splits is balanced with positive + negative transactions)
 													$value = $sec_row[0]/$sec_row[1];
 													if ((int)$value > 0){
 														echo("<td>$$value</td>");
@@ -373,20 +304,68 @@
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
+	
+	<!-- FLot Charts JavaScript -->
+	<script src="js/plugins/flot/excanvas.min.js"></script>
+	<script src="js/plugins/flot/jquery.flot.js"></script>
+	<script src="js/plugins/flot/flot-data.js"></script>
+	
     <!-- Morris Charts JavaScript -->
     <script src="js/plugins/morris/raphael.min.js"></script>
     <script src="js/plugins/morris/morris.min.js"></script>
     <script src="js/plugins/morris/morris-data.js"></script>
 	
-	<!-- Custom Morris Charts JavaScript -->
-	<!--data: *inserthere*, -->
-	<?php var_dump($tempArray); ?>
-	<script type="text/javascript">
-	Morris.Donut({
-		element: 'donut-example',
-		data: <?php echo json_encode($gnuChartArray);?>
+	<!-- Generate data from PostgreSQL, ready for Morris Charts/Graphs to use -->
+	<!-- Generate Bargraph Array -->
+	<?php
+	$bargraph_inner_array = array();
+	$bargraph_array = array();
+	$emptyParentGUID = "74a4f183da91fccda3bcef482a5cc821";
+	
+	#Iterate through the list of Expense Accounts 
+	$query = "SELECT guid,name,parent_guid FROM public.accounts WHERE account_type = 'EXPENSE'";
+	$res_accounts_list = pg_query($dbconn, $query);
+	while ($account = pg_fetch_row($res_accounts_list)) {
+
+		#Find the total value of each account and add to array.
+		if($account[2] != $emptyParentGUID) {
+			$query = "SELECT SUM(value_num) FROM public.splits WHERE account_guid = ('$account[0]') AND value_num > 0";
+			$res_account_value = pg_query($dbconn, $query);
+			$value = pg_fetch_row($res_account_value);
+
+			#If the Expense Account isn't at $0, format and add to bargraph_inner_array.
+			if ($value[0] != NULL){
+				$total_value = (intval($value[0])/100);
+				
+				$bargraph_inner_array[account]=$account[1];#\n$total_value
+				$bargraph_inner_array[value]=$total_value;
+				
+				#Add the array with the current account name + value to the main bargraph array.
+				$bargraph_array[] = $bargraph_inner_array;
+			}
+		}
+	}
+	#Sort the array in descending account value
+	usort($bargraph_array, function($b, $a) {
+		return $a['value'] - $b['value'];
 	});
+	?>
+	<!-- End Generate Bargraph Array -->
+	
+	<!-- Custom Morris Charts JavaScript -->
+	
+	<script type="text/javascript">
+	Morris.Bar({
+  element: 'expense-bar',
+  data: <?php echo json_encode($bargraph_array);?>,
+  xkey: ['account'],
+  ykeys: ['value'],
+  labels: ['Account Value'],
+  resize: true,
+  gridTextSize: 12,
+  xLabelMargin: 9
+});
+
 
 	</script>
 
